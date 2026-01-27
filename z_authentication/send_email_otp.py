3ef1fe7eab7ac587191557d3555c8ecddca51d8b
Vasudevan K <teslastark03@gmail.com>
2026-01-27 13:21:07 +0530

    added resend email service

--- z_authentication/send_email_otp.py
+++ z_authentication/send_email_otp.py
@@ -2,12 +2,17 @@ from fastapi import HTTPException, APIRouter
 from authModels import EmailRequest
 from utils.db import get_db
 from utils.auth_helpers import generate_otp
-import smtplib
-from email.mime.text import MIMEText
+# import smtplib
+# from email.mime.text import MIMEText
 import os
+from dotenv import load_dotenv
 from datetime import datetime, timedelta
 import pytz
 from z_authentication.timezone_helper import make_tz_aware
+
+import resend
+
+load_dotenv()
 
 seo_router = APIRouter()
 
@@ -73,27 +78,61 @@ async def send_email_otp(data: EmailRequest):
                 "otp_expiry": expiry_dt,
                 "email_otp_attempts": attempts + 1,
                 "email_otp_last_attempt": now,
-                # clear block once we allow this request
                 "email_otp_block_until": None,
             }
         }
     )
 
-    sender = os.getenv("SMTP_EMAIL")
-    password = os.getenv("SMTP_PASSWORD")
-    
-    msg = MIMEText(f"Your OTP for resetting password is: {otp_code}\nValid for 3 minutes.")
-    msg["Subject"] = "Beauty Sanctuary Email OTP"
-    msg["From"] = sender
-    msg["To"] = data.email
+    resend.api_key = os.getenv("RESEND_API_KEY")
 
     try:
-        with smtplib.SMTP("smtp.gmail.com", 587) as server:
-            server.starttls()
-            server.login(sender, password)
-            server.sendmail(sender, data.email, msg.as_string())
+        email = resend.Emails.send({
+            "from": os.getenv("EMAIL_FROM"),
+            "to": [data.email],
+            "subject": "Password Reset OTP",
+            "html": f"""
+               <html>
+  <body style="font-family: Arial, sans-serif; text-align: center;">
+    <div style="max-width: 600px; margin: auto; padding: 20px; border: 1px solid #ddd;">
+      <h1 style="color: #4CAF50;">Password Reset Code - Ask Zyla</h1>
+      <p>Your OTP code is:</p>
+      <h2 style="color: #FF5722;">{otp_code}</h2>
+      <p>Valid for 3 minutes</p>
+      <a href="https://project-ask-zyla-live.vercel.app" 
+         style="display: inline-block; padding: 10px 20px; background-color: #4CAF50; color: white; text-decoration: none; border-radius: 5px;">
+        Visit Website
+      </a>
+    </div>
+  </body>
+</html>
+            """
+        })
     except Exception as e:
-        print("SMTP ERROR:", repr(e))
-        raise HTTPException(status_code=500, detail=f"Email error: {e}")
+        print("RESEND ERROR:", repr(e))
+        raise HTTPException(status_code=500, detail="Failed to send email")
+
 
     return {"message": "OTP sent to email", "otp_expiry": expiry_dt.isoformat()}
+
+
+
+
+
+
+
+    # sender = os.getenv("SMTP_EMAIL")
+    # password = os.getenv("SMTP_PASSWORD")
+    
+    # msg = MIMEText(f"Your OTP for resetting password is: {otp_code}\nValid for 3 minutes.")
+    # msg["Subject"] = "Beauty Sanctuary Email OTP"
+    # msg["From"] = sender
+    # msg["To"] = data.email
+
+    # try:
+    #     with smtplib.SMTP("smtp.gmail.com", 587) as server:
+    #         server.starttls()
+    #         server.login(sender, password)
+    #         server.sendmail(sender, data.email, msg.as_string())
+    # except Exception as e:
+    #     print("SMTP ERROR:", repr(e))
+    #     raise HTTPException(status_code=500, detail=f"Email error: {e}")
